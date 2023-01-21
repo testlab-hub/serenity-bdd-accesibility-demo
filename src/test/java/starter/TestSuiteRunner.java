0912e8bcaee05bee4d2b069416b1fe17c3ee011d
testlab-hub <54805219+testlab-hub@users.noreply.github.com>
2023-01-21 19:03:49 +0000

    new commit

--- src/test/java/starter/TestSuiteRunner.java
+++ src/test/java/starter/TestSuiteRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
+        //features = "src/test/resources/features/Accessibility.feature"
         features = "src/test/resources/features"
 )
-public class CucumberTestSuite {}
+public class TestSuiteRunner {}
